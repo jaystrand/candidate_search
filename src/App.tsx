a1037ab83dfc57b4f3b1d4117c643c42c10d82b1
Hydro <jermaine.strand@yahoo.com>
2024-11-04 18:27:22 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import CandidateSearch from './pages/CandidateSearch';
-import SavedCandidates from './pages/SavedCandidates';
+import { Outlet } from 'react-router-dom';
+import Nav from './components/Nav';
 
-const App: React.FC = () => (
-  <Router>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/saved-candidates">Potential Candidates</Link>
-    </nav>
-    <Routes>
-      <Route path="/" element={<CandidateSearch />} />
-      <Route path="/saved-candidates" element={<SavedCandidates />} />
-    </Routes>
-  </Router>
-);
+function App() {
+  return (
+    <>
+      <Nav />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
 
 export default App;
